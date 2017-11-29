4e0c335f2a08a0608975aeaaa4222ce79376e345
brockcp <brockpatterson@gmail.com>
2017-11-28 20:44:03 -0800

    3_retrieve_users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,24 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    response = self.class.post('/sessions', body: { "email": email, "password": password })
-    @auth = response['auth_token']
+    response = Kele.post('/sessions', body: { "email": email, "password": password })
+
+    if response && response["auth_token"]
+      @auth_token = response["auth_token"]
+      puts "#{email} has logged in"
+    else
+      puts "login failed"
+    end
+  end
+
+  def get_me #RETRIEVE CURRENT USER
+    response = Kele.get('/users/me', headers: { "authorization" => @auth_token }) #HTTPARTY REQUEST RETURNS RESPONSE OBJECT(DATA) VIA BODY METHOD. PASSING TOKEN VIA HTTPARTYS HEADERS OPTION TO PROPERLY AUTHENTICATE AGAINST BLOC API
+    JSON.parse(response.body)  #PARSE METHOD CONVERTS DATA TO RUBY HASH
   end
 
 end
